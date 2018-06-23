3f3f155c984d073294f97fa0064830bc9d0ebe7e
Dan Holloran <dtholloran@gmail.com>
2018-06-23 10:26:55 -0500

    Initial commit

---
+++ filters.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Kindling Image Forwarding - Filters
+ * @package Kindling_Image_Forwarding
+ */
+
+add_action('kindling_ready', function () {
+    /**
+     * Updates the attachment image src uri.
+     */
+    add_filter('wp_get_attachment_image_src', function ($image, $attachment_id, $size, $icon) {
+        if (!$image) {
+            return $image;
+        }
+
+        $uri = isset($image[0]) ? $image[0] : '';
+        $image[0] = kindling_image_forwarding_replace_forward_uri($uri);
+
+        return $image;
+    }, 10, 4);
+
+    /**
+     * Updates the attachment image srcset uri.
+     */
+    add_filter('wp_calculate_image_srcset', function ($sources, $size_array, $image_src, $image_meta, $attachment_id) {
+        return array_map(function ($source) {
+            $uri = isset($source['url']) ? $source['url'] : '';
+            $source['url'] = kindling_image_forwarding_replace_forward_uri($uri);
+
+            return $source;
+        }, $sources);
+    }, 10, 5);
+});
---
+++ helpers.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Kindling Image Forwarding - Helpers
+ * @package Kindling_Image_Forwarding
+ */
+
+ /**
+  * Checks if image forwarding is enabled
+  *
+  * @return boolean
+  */
+function kindling_image_forwarding_enabled()
+{
+    return (bool) apply_filters(
+        'kindling_image_forwarding_enabled',
+        config('kindling.image-forwarding.enabled')
+    );
+}
+
+ /**
+  * Replaces the forward URL if needed.
+  *
+  * @param string $uri
+  * @return string
+  */
+function kindling_image_forwarding_replace_forward_uri($uri)
+{
+    if (!kindling_image_forwarding_enabled()) {
+        return $uri;
+    }
+
+    // Let's check if the file exists before altering it.
+    // You may want to upload files while testing even
+    // though the primary access is from the forwarding site.
+    if (file_exists(str_replace(home_url('/'), ABSPATH, $uri))) {
+        return $uri;
+    }
+
+    return str_replace(
+        home_url(),
+        apply_filters(
+            'kindling_image_forwarding_get_forward_url',
+            config('kindling.image-forwarding.url') ? config('kindling.image-forwarding.url') : home_url()
+        ),
+        $uri
+    );
+}
