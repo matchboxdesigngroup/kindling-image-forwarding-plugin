4874e97a353dc6d2801b8097be15979f52c74c24
Tracey Jones <tjones@matchboxdesigngroup.com>
2021-11-16 18:27:18 +0000

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -32,15 +32,15 @@ function kindling_image_forwarding_replace_forward_uri($uri)
     // Let's check if the file exists before altering it.
     // You may want to upload files while testing even
     // though the primary access is from the forwarding site.
-    if (file_exists(str_replace(home_url('/'), ABSPATH, $uri))) {
+    if (file_exists(str_replace(network_home_url('/'), ABSPATH, $uri))) {
         return $uri;
     }
 
     return str_replace(
-        home_url(),
+        network_home_url(),
         apply_filters(
             'kindling_image_forwarding_get_forward_url',
-            config('kindling-image-forwarding.url') ? config('kindling-image-forwarding.url') : home_url()
+            config('kindling-image-forwarding.url') ? config('kindling-image-forwarding.url') : network_home_url()
         ),
         $uri
     );
